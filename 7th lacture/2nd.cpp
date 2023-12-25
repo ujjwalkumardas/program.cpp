2654dd1d3083ccda1a286e2d831b9272b7c34b19
ujjwalkumardas <ujjwalbabu7488@gmail.com>
2023-12-25 22:50:17 +0530

    ujj

---
+++ 7th lacture/2nd.cpp
@@ -0,0 +1,21 @@
+/*three greator number*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter a num.= "<<endl;
+cin>>a;
+cout<<"enter b num. = "<<endl;
+cin>>b;
+cout<<"enter c num. = "<<endl;
+cin>>c;
+if(b<a>c){
+    cout<<a;
+}
+else if(b>c){
+    cout<<b;
+}
+else{
+    cout<<c;
+}
+}
---
+++ 7th lacture/3th.cpp
@@ -0,0 +1,9 @@
+/**/
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0;
+    cout<<a<<endl;
+    int b=021;
+    cout<<b;
+}
