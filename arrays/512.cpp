d478b607db216085163ed727577673b3067b80fd
ujjwalkumardas <ujjwalbabu7488@gmail.com>
2024-03-19 23:33:40 +0530

    commit

--- arrays/512.cpp
+++ arrays/512.cpp
@@ -1,1 +1,33 @@
-/*write a program to insert */
+/*write a program to insert in an array that is already shorted in an array*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array[10];
+    
+    int num;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "enter the element in an array[" << i << "] = ";
+        cin >> num;
+        array[i] = num;
+    }
+
+    int number;
+    cout << "enter the enement shorted element is stored = ";
+    cin >> number;
+    for (int i = 0; i < 10; i++)
+    {
+        if (array[i] > number)
+        {
+            for (int j = 10 - 1; j >= i; j--)
+            {
+                array[j + 1] = array[j];
+            }
+            array[i] = number;
+            break;
+        }
+    }
+    for(int i=0;i<10;i++){
+    cout<<" , "<<array[i];}
+}
---
+++ arrays/513.cpp
@@ -0,0 +1,30 @@
+/*write a program to merge two unstored array*/
+#include<iostream>
+using namespace std;
+int main(){
+    int array[10],array1[5],array3[15];
+    for(int i=0;i<10;i++){
+        cout<<"enter the value of array of index["<<i<<"] = ";
+        cin>>array[i];
+    }
+    cout<<"enter the value of array1"<<endl;
+    for(int i=0;i<5;i++){
+        cout<<"enter the value of array1 of index["<<i<<"] = ";
+        cin>>array1[i];
+    }
+    int index=0;
+    cout<<"-----------------------------------------------------------";
+    for(int i=0;i<10;i++){
+        array3[index]=array[i];
+        index++;
+    }
+    
+    for(int i=10;i<15;i++){
+
+        array3[i]=array1[i-10];
+    }
+    for(int i=0;i<15;i++){
+        cout<<", "<<array3[i];
+    }
+    
+}
